d2958a56b7a271f67430b5cee5dccec84caf9ee5
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-03-18 19:58:39 +0500

    added multiple toast for view

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 import Toast from "./Toast.js";
 
 new Toast({ position: "top-right", text: "Hello World" });
+new Toast({ position: "top-left", text: "Hello World" });
+new Toast({ position: "top-center", text: "Hello World" });
+new Toast({ position: "bottom-right", text: "Hello World" });
+new Toast({ position: "bottom-left", text: "Hello World" });
+new Toast({ position: "bottom-center", text: "Hello World" });
 new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
-new Toast({ position: "top-right", text: "Hello World" });
+new Toast({ position: "top-left", text: "Hello World" });
+new Toast({ position: "top-center", text: "Hello World" });
